a48d15a0553b268da2c3b39d6318a622a0ec2075
Jeff Macaluso <samacaluso@vassar.edu>
2016-02-03 16:19:00 -0500

    Initial commit
    Site live and revisions needed. Make the site look less identical to
    Bridging site

---
+++ assets/config.php
@@ -0,0 +1,84 @@
+<?php
+return 
+	array(
+	
+	// CMS site ID
+	'siteID' => '14202',
+	
+	'dev' => 'new',
+
+	// Fonts
+	'typekitID' => 'gwo2bfs',
+	
+	// 'useLocalNav' => true,
+    // 'siteName' => "Latin American & Latino/a Studies",
+    'siteNameHeader'=>'<b class="u-Masthead__siteName--line1"><img src="/samacaluso/bridging/assets/images/bridging-logo-white.png" width="500" height="184" alt="Vassar College"></b> <b class="u-Masthead__siteName--line2">Celebrating the Integrated Science Commons</b>',
+    'layoutHeaderPost' => 'gallery:embed:homepage',
+	//'phpErrorReporting' => E_ALL,
+	//'debug' => true,
+    'loadSearch'=> false,	
+	
+	/**
+	 * Complete listing of configurations - turn on debug and see $configSettings
+	 *
+	
+	'cssExternalAdditional' => null,
+	'debug' => null,
+	'galleriaTheme' => "azur",
+	'galleriaVersion' => "1.3.5",
+	'hasDropdowns' => false,
+	'isLive' => "0",
+	'isLiveSite' => "0",
+	'jsExternalAdditional' => null,
+	'loadCollegeWebsite' => false,
+	'loadGA' => "0",
+	'loadResponsive' => true,
+	'loadSearch' => true,
+	'loadSpectate' => "0",
+	'loadTabs' => false,
+	'loadTypekit' => true,
+	'newsKeywords' => "Latin American Studies",
+	'notes' => "Verified CMS match",
+	'officeBoxNumber' => "222",
+	'officeEmail' => "latinamstud",
+	'officeFax' => "",
+	'officeLocationBuilding' => "Old Laundry Building",
+	'officeLocationBuildingCode' => "OLB",
+	'officeLocationRoom' => "",
+	'officePhone' => "7668",
+	'officeURL' => "",
+	'phpErrorReporting' => integer 0,
+	'recordForSite' => "a:32:{s:6:"siteID";s:5:"32864";s:13:"siteIDClaimed";s:1:"1"; …",
+	'searchCode' => "001814011871879729902:smqot3ivpoa",
+	'searchPlaceholderText' => null,
+	'siteCode' => "lals",
+	'siteDesignType' => "Academic",
+	'siteDesignedDate' => "2000-00-00",
+	'siteDesigner' => "SiteID claimed 2013-09-05",
+	'siteHealthRating' => "0",
+	'siteID' => "32864",
+	'siteIDClaimed' => "1",
+	'siteLiveURL' => "http://latinamericanstudies.vassar.edu",
+	'siteName' => "Latin American & Latino/a Studies",
+	'siteNameCMS' => "",
+	'siteNameFooter' => "",
+	'siteNameHeader' => "<b class="u-Masthead__siteName--line1">Latin American</b> <b …",
+	<b class="u-Masthead__siteName--line1">Latin American</b> <b class="u-Masthead__siteName--line2">&amp;</b> <b class="u-Masthead__siteName--line3">Latino/a Studies</b>,
+	'siteNameReportDisplayName' => null,
+	'siteNameSubhead' => "",
+	'sitePathCode' => "latinam",
+	'siteReportDisplayName' => "Latin American and Latino/a Studies QD",
+	'siteReportTitle' => "Latin American and Latino/a Studies QD",
+	'siteSkin' => null,
+	'siteStatus' => "Omit",
+	'siteType' => "Academic",
+	'typekitFade' => false,
+	'typekitFadeDuration' => null,
+	'typekitID' => "xib8icl",
+	'typekitSelector' => null,
+	'versionGamut' => "",
+	'versionjQuery' => "1.11.0",
+	'versionjQueryUI' => "1.9.1",
+	
+*/
+);
